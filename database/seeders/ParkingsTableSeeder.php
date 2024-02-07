794849023dd689ad6a933f0d415d82f9634e0021
Jules Mathieu <mathieujules12@gmail.com>
2024-02-07 15:56:41 +0100

    en cours

---
+++ database/seeders/ParkingsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ParkingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
