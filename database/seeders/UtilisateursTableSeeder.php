1840ca9f977a67526675e5908749d54fc84e1060
Liova <liovaswagg@hotmail.com>
2024-02-07 11:59:41 +0100

    Migration Users

---
+++ database/seeders/UtilisateursTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UtilisateursTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('utilisateurs')->insert([
+            'nom' => 'Hovakimyan',
+            'prenom' => 'Liova',
+            'mail' => 'liova.hovakimyan@outlook.fr',
+	        'role' => 'Administrateur'
+        ]);
+    }
+}
