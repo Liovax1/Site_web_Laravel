8c8300ddb93984d6354ae1b15bf677b887ead3c0
Liova <liovaswagg@hotmail.com>
2024-03-12 17:04:10 +0100

    API renvoyer afficheurs des parkings

--- app/Http/Controllers/ApiGaBuZoMeuController.php
+++ app/Http/Controllers/ApiGaBuZoMeuController.php
@@ -23,15 +23,22 @@ class ApiGaBuZoMeuController extends Controller
     }
 
 
+    function getInfoNoeud($idNoeud){
+        $noeudFind = NoeudLora::where('dev_eui', $idNoeud)->first();
+        if ($noeudFind == null){
+            return response()->json(['Erreur' => 'None'], 422);
+        }
+        else {
+            $parkingFind = Parking::find($noeudFind->parking_id);
+            $infos = [];
+            $nomAfficheur = NoeudLora::where('parking_id', $noeudFind->parking_id)
+            ->where('nom_noeud', 'LIKE', 'A%')
+            ->pluck('nom_noeud') // Pour récupérer uniquement la valeur de la colonne "nom_noeud" des résultats de la requete, sinon ca prend tout
+            ->first(); // Pour récupérer le premier résultat de la requete
+            return response()->json(['ID' => $noeudFind->parking_id, 'Place dispo' => $parkingFind->nombre_place_dispo, 'Noeud' => $nomAfficheur ], 200);
+        }
+        }
+      }
 
-function getInfoNoeud($idNoeud){
-    $noeudFind = NoeudLora::where('AppEUI', $idNoeud)->first();;
-    if ($noeudFind == null){
-    return response()->json(['Erreur' => 'None'], 422);
-    }
-    else {
-    $parkingFind = Parking::find($noeudFind->parking_id);
-    return response()->json(['ID' => $noeudFind->parking_id,  'Place dispo' => $parkingFind->nombre_place_dispo, 'Type' => $noeudFind->type_noeud], 200);
-    }
-    }
-}
+
+
