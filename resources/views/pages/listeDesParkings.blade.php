141ac94f32c96430f6a63e82e0fbdddb363fab5b
liova <liova@liova>
2024-04-27 01:10:10 +0200

    test

--- resources/views/pages/listeDesParkings.blade.php
+++ resources/views/pages/listeDesParkings.blade.php
@@ -19,54 +19,9 @@
                     50 places libres
                 </h2>
             </a>
-            <div class="holds-the-iframe">
-
-
-
-            <div class="container mt-5">
-    <div class="row">
-        <div class="col">
-            <div id="mapid" style="height: 50px; width: 100px;"></div>
-        </div>
-    </div>
-</div>
-
-<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-<script>
-    var mymap = L.map('mapid').setView([44.849998, -0.03333], 13);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
-
-    L.marker([44.849998, -0.03333]).addTo(mymap)
-        .bindPopup('Castillon-la-Bataille -<br> Allées de la République.')
-        .openPopup();
-</script>
             
-            
-            
-            
-            
-            
-            
-            </div>
-            <p class="text-center" style="font-size: 16px;">
-                <i class="fa fa-map-marker-alt" aria-hidden="true"></i>
-                5 Allée de la République 33350 Castillon-la-Bataille
-            </p>
             <hr>
-            <div class="row">
-                <div class="col-md-8">
-                    <p>
-                        7 <i class="fas fa-wheelchair" aria-hidden="true"></i> |
-                        18 <i class="fa fa-motorcycle" aria-hidden="true"></i> |
-                        0 <i class="fa fa-bicycle" aria-hidden="true"></i> |
-                        0 <i class="fa fa-share-alt" aria-hidden="true"></i>
-                    </p>
-                </div>
-            </div>
+            
         </div>
     </div>
 </div>
