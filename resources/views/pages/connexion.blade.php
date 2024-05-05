e2c13ab83d2e457cc1fef8d4344b9b80f2a0a881
liova <liova@liova>
2024-05-05 22:49:02 +0200

    Contour connexion

--- resources/views/pages/connexion.blade.php
+++ resources/views/pages/connexion.blade.php
@@ -1,32 +1,29 @@
-    @extends('layouts.default')
-    @section('content')
+@extends('layouts.default')
+@section('content')
 
-<br>
-<div>
-    <h1 class="text-center">Connexion</h1>
-</div>
-<br>
-<p class="text-center">Connectez-vous !</p>
-<br><br>
-
-
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-        <div class="container d-flex justify-content-center" style="min-height: 80vh;">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <h5 class="card-header text-center">Connexion</h5>
+                    <div class="card-body">
+                        <p class="text-center">Connectez-vous !</p>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Mot de passe</label>
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">Se connecter</button>
+                        </form>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="password">Mot
-                        de passe</label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
-                </div>
-                <button type="submit" class="btn btn-primary">Se
-                    connecter</button>
-            </form>
+            </div>
         </div>
+    </div>
 
-    @stop
+@stop
